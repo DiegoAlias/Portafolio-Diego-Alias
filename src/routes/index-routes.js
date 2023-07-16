a3a8794405997193b9dad9a58f11aa4504ad299b
DiegoAlias <diego_alias_m@hotmail.com>
2023-07-15 21:30:12 -0300

    firs commit

---
+++ src/routes/index-routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express"
+const router = Router()
+
+router.get("/", (req, res) => res.render("index", { title: "Inicio" }))
+
+router.get("/about", (req, res) => res.render("about", { title: "Sobre mí" }))
+
+router.get("/skill", (req, res) => res.render("skill", { title: "Habilidades" }))
+
+router.get("/achievement", (req, res) => res.render("achievement", { title: "Logros" }))
+
+router.get("/work", (req, res) => res.render("work", { title: "Proyectos" }))
+
+router.get("/myworks", (req, res) => res.render("myworks", { title: "Proyectos" }))
+
+router.get("/contact", (req, res) => res.render("contact", { title: "Contacto" }))
+
+export default router
